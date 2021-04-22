b75278f0c2d3c4e7c50000fe4ad375db7444b693
Alex Blake <cr3ch4@gmail.com>
2021-04-22 12:38:35 +0200

    old

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 
 
 sim.run({
-    sims: 1000, // qty of games
+    sims: 10000, // qty of games
     label: moment().toISOString(), // label for files
     log: false, // log output of each game to console
     save: false, // save game moves to file
@@ -11,8 +11,23 @@ sim.run({
         x_move_first: true, // will x move first
         movers: {
             'X': require('./modules/moveRandom'),
+            'O': require('./modules/moveRandom'),
+        }
+
+    }
+});
+
+sim.run({
+    sims: 10000, // qty of games
+    label: moment().toISOString(), // label for files
+    log: false, // log output of each game to console
+    save: false, // save game moves to file
+    game_options: {
+        x_move_first: true, // will x move first
+        movers: {
+            'X': require('./modules/movePriorityChoice'),
             'O': require('./modules/movePriorityChoice'),
         }
 
     }
-})
+});
